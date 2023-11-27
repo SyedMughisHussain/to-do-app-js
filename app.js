94c6bf69dcee7620aa33f042c4e18dfd29b175ba
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-11-27 22:26:31 +0500

    Comments added for code understanding.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const todo_container = document.querySelector(".todos");
 const clearAllTodo_btn = document.querySelector(".remove-btn");
 const para = document.querySelector(".paragra");
 
+// For adding a new todo task.
 function addToDo() {
   if (input_Value.value === "") {
     alert("Please enter correct todo.");
@@ -23,25 +24,30 @@ function addToDo() {
   }
 }
 
+// Function for removing all todo's from div.
 function removeAllTodo() {
   todo_container.innerHTML = "";
   para.innerHTML = `You have 0 pending tasks`;
 }
 
+// Function for clearing input values after adding todo.
 function clearInputValues() {
   input_Value.value = "";
 }
 
+// Function for finding length of pending tasks.
 function pendingTaskLength() {
   const todoLength = todo_container.childNodes.length - 1;
   para.innerHTML = `You have ${todoLength} pending tasks`;
 }
 
+// Function for remove a single todo.
 function removeTodo(value) {
   value.parentElement.parentElement.remove();
   pendingTaskLength();
 }
 
+// Function for edit a todo with a new text.
 function editTodo(value) {
   const input = value.parentElement.parentElement;
   let newValue = prompt(
@@ -51,5 +57,6 @@ function editTodo(value) {
   console.log(input.firstElementChild.innerHTML);
 }
 
+// Button listeners for adding and removing all tasks.
 submit_btn.addEventListener("click", addToDo);
 clearAllTodo_btn.addEventListener("click", removeAllTodo);
